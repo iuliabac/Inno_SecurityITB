e93227df2c1c4e30e2590803561941a760831020
iuliabac <baca0003@hz.nl>
2025-05-30 09:13:11 +0200

    Security part done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Welcome</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body class="antialiased text-center p-10">
-<h1 class="text-4xl font-bold mb-4">Welcome to the Laravel App</h1>
-@auth
-    <p>Hello, {{ Auth::user()->name }}!</p>
-    <a href="{{ route('dashboard') }}" class="text-blue-500">Go to Dashboard</a>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <button type="submit" class="ml-4 text-red-500">Logout</button>
-    </form>
-@else
-    <a href="{{ route('login') }}" class="text-blue-500 mr-4">Login</a>
-    <a href="{{ route('register') }}" class="text-green-500">Register</a>
-@endauth
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container text-center">
+        <h1 class="display-4 mb-4">Welcome to the Laravel App</h1>
+
+        @auth
+            <p>Hello, {{ Auth::user()->name }}!</p>
+            <a href="{{ route('dashboard') }}" class="btn btn-success">Go to Dashboard</a>
+        @else
+            <a href="{{ route('login') }}" class="btn btn-primary me-2">Login</a>
+            <a href="{{ route('register') }}" class="btn btn-outline-primary">Register</a>
+        @endauth
+    </div>
+@endsection
