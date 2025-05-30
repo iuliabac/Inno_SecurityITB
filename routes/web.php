a6e8794187c87cc60fc1e56ebc9c198e8acf0beb
iuliabac <baca0003@hz.nl>
2025-05-30 10:04:05 +0200

    added a forgot password blade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
 Route::get('/', function () {
     return view('welcome');
